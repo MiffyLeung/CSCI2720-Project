aeccf0ae5bf10c922ee50b7f7f2a87af47c24db9
Tang Yuk Yee <tangyukyee@TangdeMacBook-Pro.local>
2024-12-17 23:35:03 +0800

    dev

--- frontend/src/components/CommentsSection.tsx
+++ frontend/src/components/CommentsSection.tsx
@@ -35,11 +35,12 @@ const CommentsSection: React.FC<CommentsSectionProps> = ({ initialComments = [],
 
         try {
             const data: Comment = await apiRequest(`/programme/${programmeId}/comments`, {
+    
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ text: newComment }), // Send the comment text
             });
-
+console.log("hi");
             setComments((prevComments) => [...prevComments, data]); // Add the new comment to the list
             setNewComment(''); // Clear the input
         } catch (error) {
